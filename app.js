62b24a1de80e26903c0145920613cfbafeb5e67a
Dani Vicario <danigv.email@gmail.com>
2019-01-17 10:43:08 +0100

    separation of concerns

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = {
+    init: function () {
+        var canvasDOM = document.querySelector("#canvasExp")
+
+        canvasDOM
+            .setAttribute("width", window.innerWidth);
+
+        canvasDOM
+            .setAttribute("height", window.innerHeight)
+    }
+}
