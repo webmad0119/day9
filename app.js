a11bc0e513c233a859d22a8c1a1f99abb124f281
Dani Vicario <danigv.email@gmail.com>
2019-01-17 10:59:34 +0100

    set basic app skeleton

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var app = {
-    init: function () {
-        var canvasDOM = document.querySelector("#canvasExp")
-
-        canvasDOM
+    version: '1.0',
+    canvasDOM: undefined,
+    _setCanvasDimensions: function () {
+        this.canvasDOM
             .setAttribute("width", window.innerWidth);
 
-        canvasDOM
+        this.canvasDOM
             .setAttribute("height", window.innerHeight)
+    },
+    _setHandlers : function () {
+        //remember, arrow functions doesnt change the "this" value
+        window.onresize = () => {
+            this._setCanvasDimensions()
+        }
+    },
+    init: function (canvasSelector) {
+        this.canvasDOM = document.querySelector(canvasSelector)
+
+        this._setCanvasDimensions()
+        this._setHandlers()
     }
 }
